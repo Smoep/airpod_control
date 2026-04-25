376eadc75a31c5924fb39caed28293ad04df00b1
Jos <b1t@mailbox.org>
2026-04-25 18:30:16 +0700

    Initial Commit

---
+++ airpod_control/airpod_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  airpod_controlApp.swift
+//  airpod_control
+//
+//  Created by Jos on 25/4/26.
+//
+
+import SwiftUI
+
+@main
+struct airpod_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ airpod_controlTests/airpod_controlTests.swift
@@ -0,0 +1,19 @@
+//
+//  airpod_controlTests.swift
+//  airpod_controlTests
+//
+//  Created by Jos on 25/4/26.
+//
+
+import Testing
+@testable import airpod_control
+
+struct airpod_controlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
